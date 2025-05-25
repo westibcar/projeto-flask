b312a4cdf210d8170d2367ffccefc15b52134d78
Wesley Tibucio <wesley.tibucio@gmail.com>
2025-05-25 20:19:52 -0300

    Version 1.0.2

--- uday_app/app.py
+++ uday_app/app.py
@@ -10,7 +10,7 @@ app.register_blueprint(bp)
 def health_check():
     return jsonify({
         "Status": "OK",
-        "Version": "1.0.1",
+        "Version": "1.0.2",
         "Tempo": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
         "Author": "Uday",
     }), 200
